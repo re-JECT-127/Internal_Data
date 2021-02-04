44c32642575120c33716a2ffe022ac3cc031c173
BuildTools <unconfigured@null.spigotmc.org>
2021-02-04 11:32:27 +0200

    Initial commit

---
+++ app/src/main/java/com/example/internaldata/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.example.internaldata
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val firstFragment = FirstFragment()
+        val secondFragment = SecondFragment()
+
+        supportFragmentManager.beginTransaction().apply {
+         replace(R.id.flFragment, firstFragment)
+            commit()
+        }
+
+        val btnFragment1 = findViewById<Button>(R.id.btnFragment1)
+        btnFragment1.setOnClickListener{
+            supportFragmentManager.beginTransaction().apply {
+                replace(R.id.flFragment, firstFragment)
+                addToBackStack(null)
+                commit()
+            }
+        }
+        val btnFragment2 = findViewById<Button>(R.id.btnFragment2)
+        btnFragment2.setOnClickListener{
+            supportFragmentManager.beginTransaction().apply {
+                replace(R.id.flFragment, secondFragment)
+                addToBackStack(null)
+                commit()
+            }
+        }
+    }
+}
